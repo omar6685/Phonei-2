ca4926a62b1d8ecdf1fcb60f717f79ceb8014180
omar6685 <omr6685@hotmail.com>
2022-09-21 19:52:32 +0300

    try to solve heroku problme

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 if Rails.application.credentials[:stripe].present? && Rails.application.credentials[:stripe][:secret].present?
-    Stripe.api_key = Rails.application.credentials[:stripe][:secret]
+    Stripe.api_key = 'sk_test_51LZDGTCd3RiW6igw7nTD6ACu87FKxGZhYlFmxGmp4ZN4LXggnpfdESHCTyjyvB0eLz5pJ6kNVa0hLR1Ng9jTHdse00WI2hbzaC'
 end
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(

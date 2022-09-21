23f58520d10754bb9fede39acf168cb076577fb4
omar6685 <omr6685@hotmail.com>
2022-09-21 23:51:53 +0300

    solve the problem-2

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,23 +10,23 @@ class Product < ApplicationRecord
 
   
   # create stripe product and assign to this product
-  #after_create do
-  #  product = Stripe::Product.create(name: name)
-  #  price = Stripe::Price.create(product: product, unit_amount: self.price, currency: "usd")
-  #  update(stripe_product_id: product.id, stripe_price_id: price.id)
-  #end
+  after_create do
+    product = Stripe::Product.create(name: name)
+    price = Stripe::Price.create(product: product, unit_amount: self.price, currency: "usd")
+    update(stripe_product_id: product.id, stripe_price_id: price.id)
+  end
 
   # stripe keeps products and prices separately. 
   # updating local prices would require to update or create stripe prices.
   # this is all additional complexity.
   # better just create new products.
 
-  #after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
+  after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
   # after_update :create_and_assign_new_stripe_price, if: :saved_change_to_currency?
 
-  #def create_and_assign_new_stripe_price
-  #  price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: self.currency)
-  #  update(stripe_price_id: price.id)
-  #end
+  def create_and_assign_new_stripe_price
+    price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: self.currency)
+    update(stripe_price_id: price.id)
+  end
 
 end

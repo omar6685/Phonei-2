f8809175f35a4151668d4d68e4d43b268b212959
omar6685 <omr6685@hotmail.com>
2022-09-21 15:13:57 +0300

    recreating the entire app from the beginning

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,32 @@
+class CheckoutController < ApplicationController
+  
+  def create
+    product = Product.find(params[:id])
+    @session = Stripe::Checkout::Session.create({
+      customer: current_user.stripe_customer_id,
+      payment_method_types: ['card'],
+      line_items: [{
+         price: product.stripe_price_id,
+         quantity: 1
+      }],
+      allow_promotion_codes: true,
+      mode: 'payment',
+       success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+       cancel_url: cancel_url,
+      })
+    redirect_to @session.url
+    
+  end
+
+  def success
+    @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"]})
+    @session_with_expand.line_items.data.each do |line_item|
+      product = Product.find_by(stripe_product_id: line_item.price.product)
+    end
+  end
+
+  def cancel
+  end
+
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "products#index"
+  resources :products
+  post "checkout/create", to: "checkout#create"
+  resources :webhooks, only: [:create]
+  get "success", to: "checkout#success"
+  get "cancel", to: "checkout#cancel"
+
+end
